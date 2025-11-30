fe2d8ba404cccb1bb274ce8077265ae707b3eb8f
KaiesoKB <F@irytail6>
2025-11-29 21:14:41 -0400

    Updated orders dataset

--- SQL_Scripts/table_loader.sql
+++ SQL_Scripts/table_loader.sql
@@ -1,1 +1,49 @@
 USE ecommerce_delivery;
+
+#Loading data into customers table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/customers.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(customer_unique_id, customer_id, customer_zip_code_prefix, customer_city, customer_state);
+
+SELECT COUNT(*) 
+FROM customers;
+# 93763 rows loaded
+
+#Loading data into products table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(product_id, product_category_name, product_name_length, product_description_length, product_photos_qty, product_weight_g, product_length_cm, product_height_cm, product_width_cm);
+
+SELECT COUNT(*) 
+FROM products;
+# 31296 rows loaded
+
+#Loading data into sellers table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sellers.csv'
+INTO TABLE sellers
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(seller_id, seller_city, seller_state, seller_zip_code_prefix);
+
+SELECT COUNT(*) 
+FROM sellers;
+# 2901 rows loaded
+
+#Loading data into orders table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders.csv'
+INTO TABLE orders
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_id, customer_unique_id, order_status, order_purchase_timestamp, order_approved_at, order_delivered_carrier_date, order_delivered_customer_date, order_estimated_delivery_date);
