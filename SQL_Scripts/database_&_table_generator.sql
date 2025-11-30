6cbb45c5329c5a11dad9db8fbe5229064514c61b
KaiesoKB <F@irytail6>
2025-11-29 21:25:05 -0400

    Loaded all data into database tables

--- SQL_Scripts/database_&_table_generator.sql
+++ SQL_Scripts/database_&_table_generator.sql
@@ -3,8 +3,8 @@ USE ecommerce_delivery;
 
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
+	customer_unique_id CHAR(32) PRIMARY KEY,
 	customer_id CHAR(32),
-    customer_unique_id CHAR(32),
     customer_zip_code_prefix INT,
     customer_city CHAR(32),
     customer_state CHAR(2)
@@ -12,7 +12,7 @@ CREATE TABLE customers (
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-	product_id CHAR(32),
+	product_id CHAR(32) PRIMARY KEY,
     product_category_name CHAR(40),
     product_name_length DECIMAL(4, 2),
     product_description_length DECIMAL(6, 2),
@@ -25,7 +25,7 @@ CREATE TABLE products (
 
 DROP TABLE IF EXISTS sellers;
 CREATE TABLE sellers (
-	seller_id CHAR(32),
+	seller_id CHAR(32) PRIMARY KEY,
     seller_city CHAR(40),
     seller_state CHAR(2),
     seller_zip_code_prefix INT
@@ -33,36 +33,42 @@ CREATE TABLE sellers (
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
-	order_id CHAR(32),
-    customer_id CHAR(32),
+	order_id CHAR(32) PRIMARY KEY,
+    customer_unique_id CHAR(32),
     order_status CHAR(9),
     order_purchase_timestamp DATETIME,
     order_approved_at DATETIME,
     order_delivered_carrier_date DATETIME,
     order_delivered_customer_date DATETIME,
-    order_estimated_delivery_date DATE
+    order_estimated_delivery_date DATE,
+    CONSTRAINT fk_customer FOREIGN KEY (customer_unique_id) REFERENCES customers (customer_unique_id)
 );
 
 DROP TABLE IF EXISTS orders_duration;
 CREATE TABLE orders_duration (
-	order_id CHAR(32),
+	order_id CHAR(32) PRIMARY KEY,
     apporval_time_mins INT,
     carrier_pickup_time_mins INT,
     shipping_time_mins INT,
     total_delivery_time_mins INT,
     order_delay_days INT,
-    order_delay_time_mins INT
+    order_delay_time_mins INT,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 
 DROP TABLE IF EXISTS order_items;
 CREATE TABLE order_items (
-	order_id CHAR(32),
-    order_item_id INT,
+	order_id CHAR(32) NOT NULL,
+    order_item_id INT NOT NULL,
     product_id CHAR(32),
     seller_id CHAR(32),
     shipping_limit_date DATETIME,
     price DECIMAL(6, 2),
-    freight_value DECIMAL(5, 2)
+    freight_value DECIMAL(5, 2),
+    PRIMARY KEY (order_id, order_item_id),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (seller_id) REFERENCES sellers (seller_id)
 );
 
 DROP TABLE IF EXISTS payments;
@@ -71,5 +77,7 @@ CREATE TABLE payments (
     payment_sequential INT,
     payment_type CHAR(12),
     payment_installments INT,
-    payment_value DECIMAL(7, 2)
+    payment_value DECIMAL(7, 2),
+    PRIMARY KEY (order_id, payment_sequential),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
--- SQL_Scripts/table_loader.sql
+++ SQL_Scripts/table_loader.sql
@@ -1,6 +1,6 @@
 USE ecommerce_delivery;
 
-#Loading data into customers table
+# Loading data into customers table
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/customers.csv'
 INTO TABLE customers
 FIELDS TERMINATED BY ','
@@ -13,7 +13,7 @@ SELECT COUNT(*)
 FROM customers;
 # 93763 rows loaded
 
-#Loading data into products table
+# Loading data into products table
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/products.csv'
 INTO TABLE products
 FIELDS TERMINATED BY ','
@@ -26,7 +26,7 @@ SELECT COUNT(*)
 FROM products;
 # 31296 rows loaded
 
-#Loading data into sellers table
+# Loading data into sellers table
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/sellers.csv'
 INTO TABLE sellers
 FIELDS TERMINATED BY ','
@@ -39,7 +39,7 @@ SELECT COUNT(*)
 FROM sellers;
 # 2901 rows loaded
 
-#Loading data into orders table
+# Loading data into orders table
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders.csv'
 INTO TABLE orders
 FIELDS TERMINATED BY ','
@@ -47,3 +47,46 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (order_id, customer_unique_id, order_status, order_purchase_timestamp, order_approved_at, order_delivered_carrier_date, order_delivered_customer_date, order_estimated_delivery_date);
+
+SELECT COUNT(*) 
+FROM orders;
+# 93763 rows loaded
+
+# Loading data into orders_duration table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/orders_duration.csv'
+INTO TABLE orders_duration
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_id, apporval_time_mins, carrier_pickup_time_mins, shipping_time_mins, total_delivery_time_mins, order_delay_days, order_delay_time_mins);
+
+SELECT COUNT(*) 
+FROM orders_duration;
+# 93763 rows loaded
+
+# Loading data into order_items table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/order_items.csv'
+INTO TABLE order_items
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_id, order_item_id, product_id, seller_id, shipping_limit_date, price, freight_value);
+
+SELECT COUNT(*) 
+FROM order_items;
+# 107058 rows loaded
+
+# Loading data into payments table
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/payments.csv'
+INTO TABLE payments
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(order_id, payment_sequential, payment_type, payment_installments, payment_value);
+
+SELECT COUNT(*) 
+FROM payments;
+# 97913 rows loaded
